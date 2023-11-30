8deaeab8ac71e74ffeb1a68245db622ea90889e5
Ubuntu <ubuntu@ip-10-0-3-45.ec2.internal>
2023-11-30 11:51:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 #Install gh
 
-
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 
 type -p gh >/dev/null && echo Found gh || (curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -19,3 +18,40 @@ source ${HOME}/venv/bin/activate
 pip install -U pip setuptools wheel
 pip install --upgrade torch huggingface transformers transformers[torch] trl
 pip list
+
+cd ${HOME}
+disks=$(lsblk -d | grep disk | awk '{print $1}' | sort)
+mountpoint=0
+setup_hf=0
+for d in ${disks}
+do
+	echo Checking disk ${d}
+	inuse=$(mount -v | grep ${d} | sort | wc -l)
+	if [ "${inuse}" == "0" ]; then 
+	       	echo "Disk ${d} not mounted. Checking for fylesystem"
+		fstype=$(blkid -o value -s TYPE /dev/nvme1n1)
+		if [ "${fstype}" == "" ]; then
+			echo "Disk ${d} not mounted and not formatted"
+			sudo mkfs.xfs -f ${d}
+		else
+			echo "Disk ${d} not mounted and formatted for ${fstype}"
+		fi
+		sudo mkdir /mnt/disk${mountpoint}
+		sudo mount ${d} /mnt/disk${mountpoint}
+		sudo chown -R ${USER} /mnt/disk${mountpoint}
+		echo Disk ${d} mounted on  /mnt/disk${mountpoint}
+		if [ "${setup_hf}" == "0" ]; then	
+			cache_hf_dir=${HOME}/.cache/huggingface
+			echo Setting up hf on new disk ${d} /mnt/disk${mountpoint}/huggingface
+			[ ! -d /mnt/disk${mountpoint}/huggingface ] && mkdir /mnt/disk${mountpoint}/huggingface
+			[ -d ${HOME}/.cache/huggingface ] && mv ${HOME}/.cache/huggingface/ /mnt/disk${mountpoint}/huggingface
+			ln -s /mnt/disk${mountpoint}/huggingface ${HOME}/.cache/huggingface
+			ls -l ${HOME}/.cache
+			setup_hf=1
+		fi
+
+		mountpoint=$((mountpoint+1)) 
+	else
+		echo Disk already inuse ${inuse} ignore
+	fi
+done
